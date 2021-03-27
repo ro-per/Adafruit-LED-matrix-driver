4f76b153ad1b4058ebb295a6f83c3bdc9b5e68ae
Romeo Permentier <romeo.permentier@gmail.com>
2021-03-27 11:47:09 -0400

    still building

--- src/main.rs
+++ src/main.rs
@@ -685,15 +685,18 @@ pub fn main() {
                 let row_bits : u32 = GPIO::get_row_bits(&mut io, row_loop as u8);
 
                 GPIO::clear_bits(&mut io, color_clk_mask);
-                io.write_masked_bits(row_bits, io.row_mask);
-                GPIO::set_bits(&mut io, GPIO_BIT!(PIN_LAT));
-                GPIO::clear_bits(&mut io, GPIO_BIT!(PIN_LAT));
 
-                GPIO::clear_bits(&mut io, GPIO_BIT!(PIN_OE));
+                io.write_masked_bits(row_bits, io.row_mask);
+
+                GPIO::set_bits(&mut io, GPIO_BIT!(PIN_LAT));
+
+                //GPIO::clear_bits(&mut io, GPIO_BIT!(PIN_LAT));
+
+                //GPIO::clear_bits(&mut io, GPIO_BIT!(PIN_OE));
                 
-                timer.nanosleep(io.bitplane_timings[b] as u32);
-                //nanosleep(io.bitplane_timings[b],&timer);
-                GPIO::set_bits(&mut io, GPIO_BIT!(PIN_OE));
+                //timer.nanosleep(io.bitplane_timings[b] as u32);
+
+                //GPIO::set_bits(&mut io, GPIO_BIT!(PIN_OE));
             }
         }
     }

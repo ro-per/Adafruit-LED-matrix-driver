d5e157a4433976c2e24f6245809666ce29d83ef5
ro-per <romeo.permentier@gmail.com>
2021-04-26 15:31:43 +0200

    cleanup pixel.rs and timer.rs

--- src/util/timer.rs
+++ src/util/timer.rs
@@ -1,28 +1,32 @@
-use mmap::{MemoryMap, MapOption};
-
+// ==================================== PROJECT IMPORTS =======================================
 use crate::{TIMER_REGISTER_OFFSET,TIMER_OVERFLOW};
 use crate::mmap_bcm_register;
+// ==================================== EXTERN IMPORTS =======================================
+use mmap::{MemoryMap, MapOption};
 use shuteye::sleep;
-
 use std::time::{Duration};
-// Use this struct to implement high-precision nanosleeps
+
+// ===========================================================================
+// IMPLEMENTATION OF HIGH-PRECISION NANOSLEEPS (see section 2.5 in the assignment)
+/* NOTE/WARNING: Since the raspberry pi's timer frequency is only 1Mhz, you cannot reach full nanosecond precision here. 
+    You will have to think about how you can approximate the desired precision. 
+    Obviously, there is no perfect solution here. */
+// ===========================================================================
 pub struct Timer {
     _timemap: Option<MemoryMap>,
     timereg: *mut u32 // a raw pointer to the 1Mhz timer register (see section 2.5 in the assignment)
 }
+
 impl Timer {
+    // ==================================== UNSAFE =======================================
+
     // Reads from the 1Mhz timer register (see Section 2.5 in the assignment)
     unsafe fn read(self: &Timer) -> u32 {
-        //DONE???
-        // TODO: Implement this yourself.
-        let tijd:u32 = std::ptr::read_volatile(self.timereg);
-        tijd
+        let time:u32 = std::ptr::read_volatile(self.timereg);
+        time
     }
-
+    // ==================================== CONSTRUCTOR =======================================
     pub fn new() -> Timer {
-        //DONE???
-        // TODO: Implement this yourself.
-
         let map = mmap_bcm_register(TIMER_REGISTER_OFFSET as usize);
 
         let mut timer: Timer = Timer {
@@ -42,15 +46,11 @@ impl Timer {
         timer
     }
 
-    // High-precision sleep function (see section 2.5 in the assignment)
-    // NOTE/WARNING: Since the raspberry pi's timer frequency is only 1Mhz, 
-    // you cannot reach full nanosecond precision here. You will have to think
-    // about how you can approximate the desired precision. Obviously, there is
-    // no perfect solution here.
-    fn nanosleep( self: &Timer,  mut nanos: u32) {
-        //DONE???
-        // TODO: Implement this yourself.
 
+    // ==================================== PUBLIC FUNCTIONS =======================================
+
+    // ==================================== PRIVATE FUNCTIONS =======================================    
+    fn nanosleep( self: &Timer,  mut nanos: u32) {
         let k_jitter_allowance = 60 * 1000 + 0;
 
 		 if nanos > k_jitter_allowance{
